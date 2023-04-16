cd9eff4da686ed4457058ed94b0e82574e6f38c2
mac <mac@MacBook-Pro-de-mac.local>
2023-04-16 16:49:48 +0000

    creating sidebar topbar

--- src/theme.js
+++ src/theme.js
@@ -4,192 +4,196 @@ import { createTheme } from "@mui/material/styles";
 // color design tokens
 export const tokens = (mode) => ({
     ...(mode === "dark"
-        ? {
-            black: {
-                100: "#cccccc",
-                200: "#999999",
-                300: "#666666",
-                400: "#333333",
-                500: "#000000",
-                600: "#000000",
-                700: "#000000",
-                800: "#000000",
-                900: "#000000",
-            },
-            blue: {
-                100: "#d0d3d8",
-                200: "#a1a6b1",
-                300: "#727a8b",
-                400: "#434d64",
-                500: "#14213d",
-                600: "#101a31",
-                700: "#0c1425",
-                800: "#080d18",
-                900: "#04070c",
-            },
-            secondary: {
-                100: "#feedcf",
-                200: "#fedaa0",
-                300: "#fdc870",
-                400: "#fdb541",
-                500: "#fca311",
-                600: "#ca820e",
-                700: "#97620a",
-                800: "#654107",
-                900: "#322103",
-            },
-            grey: {
-                100: "#fafafa",
-                200: "#f5f5f5",
-                300: "#efefef",
-                400: "#eaeaea",
-                500: "#e5e5e5",
-                600: "#b7b7b7",
-                700: "#898989",
-                800: "#5c5c5c",
-                900: "#2e2e2e",
-            },
-            primary: {
-                100: "#ffffff",
-                200: "#ffffff",
-                300: "#ffffff",
-                400: "#ffffff",
-                500: "#ffffff",
-                600: "#cccccc",
-                700: "#999999",
-                800: "#666666",
-                900: "#333333",
-            }
-        } : {
-            black: {
-            },
-                100: "#000000",
-                200: "#000000",
-                300: "#000000",
-                400: "#000000",
-                500: "#000000",
-                600: "#333333",
-                700: "#666666",
-                800: "#999999",
-                900: "#cccccc",
-            blue: {
-                100: "#04070c",
-                200: "#080d18",
-                300: "#0c1425",
-                400: "#101a31",
-                500: "#14213d",
-                600: "#434d64",
-                700: "#727a8b",
-                800: "#a1a6b1",
-                900: "#d0d3d8",
-            },
-            secondary: {
-                100: "#322103",
-                200: "#654107",
-                300: "#97620a",
-                400: "#ca820e",
-                500: "#fca311",
-                600: "#fdb541",
-                700: "#fdc870",
-                800: "#fedaa0",
-                900: "#feedcf",
-            },
-            grey: {
-                100: "#2e2e2e",
-                200: "#5c5c5c",
-                300: "#898989",
-                400: "#b7b7b7",
-                500: "#e5e5e5",
-                600: "#eaeaea",
-                700: "#efefef",
-                800: "#f5f5f5",
-                900: "#fafafa",
-            },
-            primary: {
-                100: "#333333",
-                200: "#666666",
-                300: "#999999",
-                400: "#cccccc",
-                500: "#ffffff",
-                600: "#ffffff",
-                700: "#ffffff",
-                800: "#ffffff",
-                900: "#ffffff",
-            }
+      ? {
+          grey: {
+            100: "#e0e0e0",
+            200: "#c2c2c2",
+            300: "#a3a3a3",
+            400: "#858585",
+            500: "#666666",
+            600: "#525252",
+            700: "#3d3d3d",
+            800: "#292929",
+            900: "#141414",
+          },
+          primary: {
+            100: "#d0d1d5",
+            200: "#a1a4ab",
+            300: "#727681",
+            400: "#1F2A40",
+            500: "#141b2d",
+            600: "#101624",
+            700: "#0c101b",
+            800: "#080b12",
+            900: "#040509",
+          },
+          greenAccent: {
+            100: "#dbf5ee",
+            200: "#b7ebde",
+            300: "#94e2cd",
+            400: "#70d8bd",
+            500: "#4cceac",
+            600: "#3da58a",
+            700: "#2e7c67",
+            800: "#1e5245",
+            900: "#0f2922",
+          },
+          redAccent: {
+            100: "#f8dcdb",
+            200: "#f1b9b7",
+            300: "#e99592",
+            400: "#e2726e",
+            500: "#db4f4a",
+            600: "#af3f3b",
+            700: "#832f2c",
+            800: "#58201e",
+            900: "#2c100f",
+          },
+          blueAccent: {
+            100: "#e1e2fe",
+            200: "#c3c6fd",
+            300: "#a4a9fc",
+            400: "#868dfb",
+            500: "#6870fa",
+            600: "#535ac8",
+            700: "#3e4396",
+            800: "#2a2d64",
+            900: "#151632",
+          },
         }
-    )
-})
+      : {
+          grey: {
+            100: "#141414",
+            200: "#292929",
+            300: "#3d3d3d",
+            400: "#525252",
+            500: "#666666",
+            600: "#858585",
+            700: "#a3a3a3",
+            800: "#c2c2c2",
+            900: "#e0e0e0",
+          },
+          primary: {
+            100: "#040509",
+            200: "#080b12",
+            300: "#0c101b",
+            400: "#f2f0f0", // manually changed
+            500: "#141b2d",
+            600: "#1F2A40",
+            700: "#727681",
+            800: "#a1a4ab",
+            900: "#d0d1d5",
+          },
+          greenAccent: {
+            100: "#0f2922",
+            200: "#1e5245",
+            300: "#2e7c67",
+            400: "#3da58a",
+            500: "#4cceac",
+            600: "#70d8bd",
+            700: "#94e2cd",
+            800: "#b7ebde",
+            900: "#dbf5ee",
+          },
+          redAccent: {
+            100: "#2c100f",
+            200: "#58201e",
+            300: "#832f2c",
+            400: "#af3f3b",
+            500: "#db4f4a",
+            600: "#e2726e",
+            700: "#e99592",
+            800: "#f1b9b7",
+            900: "#f8dcdb",
+          },
+          blueAccent: {
+            100: "#151632",
+            200: "#2a2d64",
+            300: "#3e4396",
+            400: "#535ac8",
+            500: "#6870fa",
+            600: "#868dfb",
+            700: "#a4a9fc",
+            800: "#c3c6fd",
+            900: "#e1e2fe",
+          },
+        }),
+  });
 
-//mui theme settings
 
+// mui theme settings
 export const themeSettings = (mode) => {
-    const colors = tokens (mode);
+    const colors = tokens(mode);
     return {
-        palette: {
-            mode: mode,
-            ...(mode === 'dark'
-                ? {
-                    primary: {
-                        main: colors.primary[500],
-                    },
-                    secondary: {
-                        main: colors.greenAccent [500],
-                    },
-                    neutral: {
-                        dark: colors.grey [700],
-                        main: colors.grey [500],
-                        light: colors.grey[100]
-                    },
-                    background: {
-                        default: colors.primary[500],
-                    }
-                } : {
-                    primary: {
-                        main: colors.primary[100],
-                    },
-                    secondary: {
-                        main: colors.secondary[500],
-                    },
-                    neutral: {
-                        dark: colors.grey [700],
-                        main: colors.grey [500],
-                        light: colors.grey[100]
-                    },
-                    background: {
-                        default: colors.black[500],
-                    }
-                }), 
+      palette: {
+        mode: mode,
+        ...(mode === "dark"
+          ? {
+              // palette values for dark mode
+              primary: {
+                main: colors.primary[500],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                dark: colors.grey[700],
+                main: colors.grey[500],
+                light: colors.grey[100],
+              },
+              background: {
+                default: colors.primary[500],
+              },
+            }
+          : {
+              // palette values for light mode
+              primary: {
+                main: colors.primary[100],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                dark: colors.grey[700],
+                main: colors.grey[500],
+                light: colors.grey[100],
+              },
+              background: {
+                default: "#fcfcfc",
+              },
+            }),
+      },
+      typography: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 12,
+        h1: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 40,
         },
-        typography: {
-            fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
-            fontSize: 40,
-            h1: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 40,
-            },
-            h2: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 32,
-            },
-            h3: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 24,
-            },
-            h4: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 20,
-            },
-            h5: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 16,
-            },
-            h6: {
-                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-                fontSize: 14,
-            },
+        h2: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 32,
         },
+        h3: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 24,
+        },
+        h4: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 20,
+        },
+        h5: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 16,
+        },
+        h6: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 14,
+        },
+      },
     };
-};
+  };
+  
 
 // context for color mode
 export const ColorModeContext = createContext({
